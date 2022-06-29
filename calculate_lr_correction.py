7117f1451900fa0d52456451d0f4f1d67b1e87a9
surajit_n <surajit.nandi1988@gmail.com>
2022-06-29 19:25:19 +0200

    minor

--- calculate_lr_correction.py
+++ calculate_lr_correction.py
@@ -5,13 +5,6 @@ import glob
 import pandas as pd
 
 
-all_bonds = ['C_s_C', 'C_d_C', 'C_t_C', 'C_C_A', 'C_s_H', 'C_s_O',
-              'C_d_O', 'C_t_O', 'C_O_A', 'C_s_N', 'C_d_N', 'C_t_N',
-              'C_N_A', 'C_s_F', 'O_s_O', 'O_d_O', 'O_O_A', 'O_s_H',
-              'O_s_N', 'O_d_N', 'O_t_N', 'O_N_A', 'O_s_F', 'N_s_N',
-              'N_d_N', 'N_t_N', 'N_N_A', 'N_s_H', 'N_s_F', 'F_s_H']
-
-
 def get_csv_files(csv_dir):
     """This function returns a list of csv files with matching pattern Reaction_*.csv."""
     files = []
@@ -23,14 +16,17 @@ def get_csv_files(csv_dir):
     return files
 
 
-def get_energy_data(csv_files, dft_functional, bonds_coeffs = None):
+def get_energy_data(csv_files, dft_functional, bonds_coeffs_dict = None):
     """Returns:
                dE, dE_corrected
     """
     dE = []
     dE_corrected = []
+    reactantindex = []
+    pdtindex = []
     chunksize = 100000
-    print("bonds list: ", bonds_coeffs)
+    bonds_list = list(bonds_coeffs_dict.keys())
+    print("bonds list: ", bonds_list)
     print("Starting to read csv files")
     for csvs in csv_files:
         nchunk =0
@@ -39,24 +35,26 @@ def get_energy_data(csv_files, dft_functional, bonds_coeffs = None):
             nchunk += 1
             df = chunk.dropna(axis=0, how='any')
             # remove all the reactions where there is no bond changes:
-            df = df.loc[(df[all_bonds].abs().sum(axis=1) != 0)]
+            df = df.loc[(df[bonds_list].abs().sum(axis=1) != 0)]
             dE_np = df.loc[:, ("G4MP2")] - df.loc[:, (dft_functional.upper())].to_numpy()
             dE += dE_np.to_list()
-            for bond, coef in bonds_coeffs.items():
+            reactantindex += df["reactantindex"].to_list()
+            pdtindex += df["pdtindex"].to_list()
+            for bond, coef in bonds_coeffs_dict.items():
                 bond_value = df[bond].to_numpy()
                 dE_np -= coef * bond_value
             dE_corrected += dE_np.to_list()
             if nchunk%10 == 0:
                 print("Nchunk: ", nchunk)
-    return dE, dE_corrected
+    return reactantindex, pdtindex, dE, dE_corrected
 
 
-def save_to_csv(dE, dE_corrected, dft_functional, output=None):
+def save_to_csv(dE, dE_corrected, reactindex, pdtindex, dft_functional, output=None):
     print("Writing energy correction data to csv file: ", output)
     with open(output, 'w') as fp:
         writer = csv.writer(fp)
-        writer.writerow(["dE_" + str(dft_functional), "dE_corrected_" + str(dft_functional)])
-        writer.writerows(zip(dE, dE_corrected))
+        writer.writerow(["reactindex", "pdtindex", "dE_" + str(dft_functional), "dE_corrected_" + str(dft_functional)])
+        writer.writerows(zip(reactindex, pdtindex, dE, dE_corrected))
     return
 
 
@@ -88,8 +86,9 @@ def main():
     dft_functional = args.dft_functional
     csv_files = get_csv_files(args.csv_directory)
     bonds_lr_pd = pd.read_csv(args.lr_coeff, index_col=False)
-    dE, dE_corrected = get_energy_data(csv_files, dft_functional, bonds_coeffs=dict(zip(bonds_lr_pd["bonds"], bonds_lr_pd["coefficients"])))
-    save_to_csv(dE, dE_corrected, dft_functional, output=output+".csv")
+    bonds_coeffs_dict = dict(zip(bonds_lr_pd.bonds, bonds_lr_pd.coefficients))
+    reactindex, pdtindex, dE, dE_corrected = get_energy_data(csv_files, dft_functional, bonds_coeffs_dict=bonds_coeffs_dict)
+    save_to_csv(dE, dE_corrected, reactindex, pdtindex, dft_functional, output=output+".csv")
     pass
 
 
